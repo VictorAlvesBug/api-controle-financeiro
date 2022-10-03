29a59d33ec00c4b1cc62acdc1268b6bec51fbc4a
Victor Alves Bugueno <victor.bugueno@fiap.com.br>
2022-10-03 15:34:39 -0300

    Adicionado tratamento de CORS

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./config/express')();
+const cors = require('cors');
 const port = app.get('port');
+
+
+app.use(cors({origin:true}))
 
 // Rodando nossa aplicação na porta setada
 app.listen(port, () => {
